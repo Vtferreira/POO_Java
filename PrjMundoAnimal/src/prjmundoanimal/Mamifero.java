a1a383326e8bcd3e3ee75352ac67168fa047c9d6
tfvinicius <tfvinicius@gmail.com>
2017-08-11 22:21:47 -0300

    Aula 11/08 - Criando nova classe

---
+++ PrjMundoAnimal/src/prjmundoanimal/Mamifero.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ * class Mamifero extends Animal
+ */
+package prjmundoanimal;
+
+/**
+ *
+ * @author aluno
+ */
+public class Mamifero extends Animal {
+    private int tamanho;
+    private String porte;
+    
+    public Mamifero(){
+        
+    }
+    
+    Mamifero rato = new Mamifero();
+    
+    
+    
+    
+    
+    
+}
--- PrjMundoAnimal/src/prjmundoanimal/PrjMundoAnimal.java
+++ PrjMundoAnimal/src/prjmundoanimal/PrjMundoAnimal.java
@@ -2,40 +2,23 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+
  */
 package prjmundoanimal;
 
 /**
  *
- * @author aluno
+ * @author vinicius
  */
 public class PrjMundoAnimal {
 
-    
     public static void main(String[] args) {
         
         Animal edmundoAnimal = new Animal(); //instancia de objeto
         
-        //masculino
+        
         edmundoAnimal.setSexoAnimal("transgenero"); // definindo o sexo
         String resultado = edmundoAnimal.getSexoAnimal(); //obtemos o sexo
         System.out.println(resultado + " " + ("transgenero" == resultado)); // imprimimos o sexo
-        
-        edmundoAnimal.setSexoAnimal("masculino"); // definindo o sexo
-        resultado = edmundoAnimal.getSexoAnimal();
-        System.out.println(resultado + " " + ("masculino" == resultado));
-        
-        edmundoAnimal.setSexoAnimal("feminino"); // definindo o sexo
-        resultado = edmundoAnimal.getSexoAnimal();
-        System.out.println(resultado + " " + ("feminino" == resultado));
-        
-        edmundoAnimal.setSexoAnimal("xxxxxxxxxxx"); // definindo o sexo
-        resultado = edmundoAnimal.getSexoAnimal();
-        System.out.println(resultado + " " + ("masculino" == resultado));
-        System.out.println(resultado + " " + ("feminino" == resultado));
-        System.out.println(resultado + " " + ("transgenero" == resultado));
-        System.out.println(resultado + " " + ("erro" == resultado));
-        
     }
-    
 }
